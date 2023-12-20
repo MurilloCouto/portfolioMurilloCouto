4af6195e8856d389039e06c4fbd082e302c71628
Murillo Alves Couto <murilloacouto@outlook.com>
2023-12-20 18:08:43 -0300

    deleting

--- src/componentes/section/Projects.js
+++ src/componentes/section/Projects.js
@@ -21,16 +21,16 @@ function Projects() {
           img='https://github.com/MurilloCouto/facetruqueFeed/blob/main/Captura%20de%20tela%202023-12-20%20175634.png?raw=true' 
           title="Facetruque" 
           tech="HTML, CSS, JS, REACT" 
-          desc="Projeto Front-end simulando um feed de uma rede social fictícia" 
+          desc="Projeto Front-end simulando um feed de uma rede social fictícia, permitindo o usuário a adicionar e remover comentários, bem como curtí-los" 
           site="https://facetruque-feed.vercel.app/" 
           repo="https://github.com/MurilloCouto/facetruqueFeed"/>
 
-          <Cards img={project3} 
-          title="Landing page" 
-          tech="HTML, CSS" 
-          desc="Projeto Front-end de uma empresa de arquitetura fictícia para captação de possíveis novos clientes" 
-          site="https://desafio-arquitetura-dnc.netlify.app/" 
-          repo="https://github.com/MurilloCouto/landing-page-arquitetura"/>
+          <Cards img="https://github.com/MurilloCouto/challenge-3/blob/main/Captura%20de%20tela%202023-12-13%20080547.png?raw=true" 
+          title="To Do List" 
+          tech="HTML, CSS, JS, REACT" 
+          desc="Projeto Front-end para criação de uma lista de tarefas, permitindo o usuário a adicionar e excluir tarefas da lista" 
+          site="https://challenge-3-hazel.vercel.app/" 
+          repo="https://github.com/MurilloCouto/challenge-3"/>
           
         <ButtonB link="https://github.com/MurilloCouto?tab=repositories" text="Acessar repositório completo"/>
       </div>
