6ccd75f5afa9c2a9c8d5461c3680b1d56dcc855f
Murillo Alves Couto <murilloacouto@outlook.com>
2024-01-17 11:13:06 -0300

    Fixing layout

--- src/componentes/section/Navbar.jsx
+++ src/componentes/section/Navbar.jsx
@@ -19,19 +19,19 @@ function Navbar() {
       <ul className={styles.list2}>
         <li>
           {" "}
-          <a href="https://www.instagram.com/murilloacouto/">
+          <a href="https://www.instagram.com/murilloacouto/" target="_blank">
             <FaInstagram size={35} />
           </a>
         </li>
         <li>
           {" "}
-          <a href="https://github.com/MurilloCouto">
+          <a href="https://github.com/MurilloCouto" target="_blank">
             <FaGithub size={35} />
           </a>
         </li>
         <li>
           {" "}
-          <a href="https://www.linkedin.com/in/murillo-alves-couto-1b072828a/">
+          <a href="https://www.linkedin.com/in/murillocouto" target="_blank">
             <FaLinkedin size={35} />
           </a>
         </li>
